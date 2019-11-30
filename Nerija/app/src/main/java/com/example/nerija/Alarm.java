542071d53576fe76420174de6f0a1abcfa6c19ec
띵떰 <namdohun1@gmail.com>
2019-11-30 23:50:37 +0900

    알람 멈춤버튼 생성 및 동작 추가 알람 최적화

--- Nerija/app/src/main/java/com/example/nerija/Alarm.java
+++ Nerija/app/src/main/java/com/example/nerija/Alarm.java
@@ -1,23 +1,21 @@
 package com.example.nerija;
 
-import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Vibrator;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.Date;
 
 public class Alarm extends Thread implements Serializable {
+
     public Date alarm_date; // 알람을 울릴 시간 정보 저장;
-    private boolean stop = false;   // 스레드 stop flag false 일때 스레드 동작
 
 
-    public void ShowTimerMethod(final Context app, final Intent intent){ // TextView는 화면에 띄워서 결과 확인하기 위한 선언; 나중에 제거하기
+    public void StartAlarm(final Context app, final Intent intent){ // TextView는 화면에 띄워서 결과 확인하기 위한 선언; 나중에 제거하기
         final Toast toast = Toast.makeText(app.getApplicationContext(), "알람 쨘",Toast.LENGTH_SHORT);
         final Vibrator vib = (Vibrator) app.getSystemService(Context.VIBRATOR_SERVICE);
 
@@ -32,7 +30,8 @@ public class Alarm extends Thread implements Serializable {
 
             @Override
             public void run() {
-                while(!stop){
+
+                while(!isInterrupted()){
                     try{
                         Thread.sleep(1000); // 1초 동안 쓰레드 정지; 나중에 분으로 바꿔도 됨
                     }
@@ -49,35 +48,23 @@ public class Alarm extends Thread implements Serializable {
 
         Thread thread = new Thread(task);   // ShowTimerMethod가 실행 되면 바로 스레드 실행
         thread.start();
+
     }
 
-    public void setStop(boolean stop){  // 스레드 멈추는 메소드
-        this.stop = stop;
-    }
-
-    public void getDiffer(final Context app, Toast toast, Vibrator vib, Intent intent){// 시간차이 구하는 메소드
+    // 시간차이 구하는 메소드
+    public void getDiffer(final Context app, Toast toast, Vibrator vib, Intent intent){
 
         long diff = new Date().getTime() - alarm_date.getTime();    // 현재시간 - 받아온 시간 => 실제로는 받아온 시간 - 현재시간 할 것
         long sec = diff/1000;   // 나누기 1000 : 초; 나누기 60000 : 분; 나누기 3600000 : 시 나중에 바꾸기;
+        long[] pattern = {1000,1000};
 
-        //여기서부터는 테스트를 위한 장난 입니다
-        if(sec <5){
-
-        }
-        else if(sec >= 15){
-            setStop(true);
-        }
-        else if(sec == 5){
+        if(sec == 5){
             toast.show();
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
             app.startActivity(intent);
+            vib.vibrate(pattern,0);
         }
 
-        else{
-            long[] pattern = {0,1000,1000};
-            vib.vibrate(pattern, -1);
-        }
-        // 장난 끝
     }
 
     public void getDate(final Date date) {  // 알람 시간 받아오는 메소드
--- Nerija/app/src/main/java/com/example/nerija/namdoActivity.java
+++ Nerija/app/src/main/java/com/example/nerija/namdoActivity.java
@@ -3,7 +3,10 @@ package com.example.nerija;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,5 +21,10 @@ public class namdoActivity extends AppCompatActivity
         setContentView(R.layout.activity_namdo);
         TextView tv = findViewById(R.id.textView);
         tv.setText("알람 울리면 나오는 페이지");
+
     }
+    public void on1(View v){
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
 }
