92aa545ed57f81b2f5cc62f56a3bab33da779de0
aycaramba <rjsxkr1122@naver.com>
2019-12-01 20:46:49 +0900

    MessageManager 수정

--- Nerija/app/src/main/java/com/example/nerija/MessageManager.java
+++ Nerija/app/src/main/java/com/example/nerija/MessageManager.java
@@ -1,44 +1,21 @@
 package com.example.nerija;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.telephony.SmsManager;
-import android.widget.Toast;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.util.ArrayList;
 
 public class MessageManager
 {
     private String messageString;
-    private NamePhoneNum receiver;
+    private String receiver;
 
-    private Context appContext;
-    private Activity activity;
-
-
-
-    public MessageManager(Activity ac)
+    public MessageManager()
     {
-        activity = ac;
 
-        this.appContext = ac.getApplicationContext();
-
-        if ( Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(appContext, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED )
-        {
-            ActivityCompat.requestPermissions(activity, new String[]{android.Manifest.permission.SEND_SMS}, 0);
-        }
 
     }
 
     public void makeMessage(String arrival)
     {
-        messageString = "[<내리자>에서 발신]\n" +receiver.getName() +"님! 지금 "  + arrival + "근처에 도착 했습니다." ;
+        messageString = "[<내리자>에서 발신]\n" +receiver +"님! 지금 "  + arrival + "근처에 도착 했습니다." ;
     }
 
     public void transmitMessage(String phoneNum)
@@ -47,21 +24,20 @@ public class MessageManager
         {
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(phoneNum, null, messageString, null, null);
-            Toast.makeText(appContext, "전송 완료!", Toast.LENGTH_LONG).show();
         }
 
         catch (Exception e)
         {
-            Toast.makeText(appContext, "SMS faild, please try again later!", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
     }
 
-    public NamePhoneNum getReceiver() {
+    public String getReceiver() {
         return receiver;
     }
-    public void setReceiver(NamePhoneNum receiver) {
-        this.receiver = receiver;
+    public void setReceiver(String receiver)
+    {
+        this.receiver = new String(receiver);
     }
 
     public String getMessageString() {
